5de4fa323e56dfbf2f48fcc51c3d9dae6bae5ab9
Changwei Zhang <chazhan@microsoft.com>
2024-01-24 18:08:16 +0800

    add openai config file

---
+++ entry/src/main/ets/pages/config.template.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+export default config = {
+  // Azure OpenAI https://xxx.openai.azure.com/openai/deployments/xxx/chat/completions?api-version=2023-07-01-preview
+  // OpenAI.com
+  endpoint: "",
+  apiKey: "",
+  // user azure openai service must be true
+  useAzure: false
+}
